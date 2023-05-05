40189827339bdaf281638e020f5651db0316576d
juanmiguel431 <juanmiguel431@gmail.com>
2023-05-04 22:29:02 -0400

    redux-devtools-extension added

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import { ActionType } from './action-types';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import * as actionCreators from './action-creators';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+const composeEnhancers = composeWithDevTools({ actionCreators });
+
+export const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(thunk)));
+
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code'
+  }
+});
+
+console.log('JMPC', store.getState());
+
+// const state = store.getState();
