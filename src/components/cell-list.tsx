44673508927a421781ed398d102537a85ffdc911
juanmiguel431 <juanmiguel431@gmail.com>
2023-05-09 21:13:27 -0400

    Force visible and using transitions with css

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -4,19 +4,19 @@ import CellListItem from './cell-list-item';
 import AddCell from './add-cell';
 
 const CellList: React.FC = () => {
-  const cells = useAppSelector(({ cells: { order, data}  }) => order.map((id) => data[id]));
+  const cells = useAppSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
 
   const renderedCells = cells.map(cell => (
-    <React.Fragment key={cell.id} >
-      <AddCell nextCellId={cell.id} />
-      <CellListItem cell={cell} />
+    <React.Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id}/>
+      <CellListItem cell={cell}/>
     </React.Fragment>
-  ) );
+  ));
 
   return (
     <div className="cell-list">
       {renderedCells}
-      <AddCell nextCellId={null} />
+      <AddCell forceVisible={cells.length === 0} nextCellId={null}/>
     </div>
   );
 }
