5740573ca5c0c8584dc9fa372fed1cae2e8423d7
juanmiguel431 <juanmiguel431@gmail.com>
2023-05-11 22:08:13 -0400

    Bundle - actions and action types added.

--- src/state/action-types/index.ts
+++ src/state/action-types/index.ts
@@ -5,4 +5,6 @@ export enum ActionType {
   INSERT_CELL_BEFORE = 'insert_cell_before',
   INSERT_CELL_AFTER = 'insert_cell_after',
   UPDATE_CELL = 'update_cell',
+  BUNDLE_START = 'bundle_start',
+  BUNDLE_COMPLETE = 'bundle_complete',
 }
