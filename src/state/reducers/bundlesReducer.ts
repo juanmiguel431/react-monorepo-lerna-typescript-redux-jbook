060055f15d86058e4d12b63455bbf3557285fea2
juanmiguel431 <juanmiguel431@gmail.com>
2023-05-11 22:23:25 -0400

    bundlesReducer implemented.

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -1,1 +1,36 @@
-export {}
+import { produce } from 'immer';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+interface BundlesState {
+  [key: string]: {
+    loading: boolean;
+    code: string;
+    err: string;
+  }
+}
+
+const initialState: BundlesState = {}
+
+const reducer = produce((state: BundlesState, action: Action) => {
+  switch (action.type) {
+    case ActionType.BUNDLE_START:
+      state[action.payload.cellId] = {
+        loading: true,
+        code: '',
+        err: '',
+      }
+      break;
+    case ActionType.BUNDLE_COMPLETE:
+      state[action.payload.cellId] = {
+        loading: false,
+        code: action.payload.bundle.code,
+        err: action.payload.bundle.err,
+      }
+      break;
+    default:
+      return state;
+  }
+}, initialState);
+
+export default reducer;
