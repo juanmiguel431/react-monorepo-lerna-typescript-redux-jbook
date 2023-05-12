92bcff0597d8947a5dce8bd0d21880e1ee3b26b5
juanmiguel431 <juanmiguel431@gmail.com>
2023-05-11 21:52:34 -0400

    example code removed.

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,30 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-import { ActionType } from './action-types';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import * as actionCreators from './action-creators';
 
 const composeEnhancers = composeWithDevTools({ actionCreators });
 
 export const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(thunk)));
-
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_BEFORE,
-  payload: {
-    id: null,
-    type: 'code'
-  }
-});
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_BEFORE,
-  payload: {
-    id: null,
-    type: 'text'
-  }
-});
 
 //https://react-redux.js.org/using-react-redux/usage-with-typescript#define-root-state-and-dispatch-types
 // Infer the `RootState` and `AppDispatch` types from the store itself
