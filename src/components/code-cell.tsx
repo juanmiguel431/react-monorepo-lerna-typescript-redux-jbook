2b9819c44d28ec748abc23312716ec8c8b2be52d
juanmiguel431 <juanmiguel431@gmail.com>
2023-05-17 22:53:58 -0400

    Putting a white background in preview window no matter what.

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -41,14 +41,16 @@ export const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             <CodeEditor initialValue={cell.content} onChange={value => updateCell(cell.id, value)}/>
           </Resizable>
 
-          {!bundle || bundle.loading ? (
-            <div className="progress-cover">
-              <progress className="progress is-small is-primary" max="100%">Loading</progress>
-            </div>
-          ) : (
-            <Preview code={bundle.code} bundlingStatus={bundle.error}/>
-          )
-          }
+          <div className="progress-wrapper">
+            {!bundle || bundle.loading ? (
+              <div className="progress-cover">
+                <progress className="progress is-small is-primary" max="100%">Loading</progress>
+              </div>
+            ) : (
+              <Preview code={bundle.code} bundlingStatus={bundle.error}/>
+            )
+            }
+          </div>
         </div>
       </Resizable>
     </div>
