cf90abc32293d72dac0c49555dcf7969dc911ad9
juanmiguel431 <juanmiguel431@gmail.com>
2023-05-21 15:56:35 -0400

    Implementing show function - no operation.

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -16,15 +16,15 @@ export const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
   const bundle = useTypedSelector(s => s.bundles[cell.id]);
 
-  const cumulativeCode = useTypedSelector(s  => {
+  const cumulativeCode = useTypedSelector(s => {
     const { data, order } = s.cells;
-    const orderedCells = order.map(id =>  data[id] );
-    const cumulativeCode = [
-      `
+    const orderedCells = order.map(id => data[id]);
+
+    const showFunc = `
         import _React from 'react';
         import _ReactDOM from 'react-dom';
         const _root = document.querySelector('#root');
-        const show = (value) => {
+        var show = (value) => {
           if (typeof value === 'object') {
             if (value.$$typeof && value.props) {
               _ReactDOM.render(value, _root);
@@ -35,10 +35,14 @@ export const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             _root.innerHTML = value;
           }
         }
-      `
-    ];
+      `;
+
+    const showFuncNoOp = 'var show = () => {}';
+
+    const cumulativeCode = [];
     for (let c of orderedCells) {
       if (c.type === 'code') {
+        cumulativeCode.push(c.id === cell.id ? showFunc : showFuncNoOp);
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
