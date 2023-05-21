1041da238f75fc409fad3f55d276fc3f916b3e57
juanmiguel431 <juanmiguel431@gmail.com>
2023-05-21 16:13:37 -0400

    Fixing Edge Snaps

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
 import AddCell from './add-cell';
+import './cell-list.css';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
