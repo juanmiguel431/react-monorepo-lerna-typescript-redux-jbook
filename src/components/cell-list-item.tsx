18add3f8b8bb15b035facc6e88138dc8407cd069
juanmiguel431 <juanmiguel431@gmail.com>
2023-05-21 16:23:42 -0400

    Using framer-motion to improve the UX when we add, move or delete cell.

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -4,6 +4,7 @@ import TextEditor from './text-editor';
 import CodeCell from './code-cell';
 import ActionBar from './action-bar';
 import './cell-list-item.css';
+import { LayoutGroup, motion } from 'framer-motion';
 
 interface CellListItemProps {
   cell: Cell;
@@ -14,15 +15,23 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
   if (cell.type === 'code') {
     child = <>
-      <div className="action-bar-wrapper">
-        <ActionBar id={cell.id}/>
-      </div>
-      <CodeCell cell={cell}/>
+      <LayoutGroup>
+        <motion.div layout>
+          <div className="action-bar-wrapper">
+            <ActionBar id={cell.id}/>
+          </div>
+          <CodeCell cell={cell}/>
+        </motion.div>
+      </LayoutGroup>
     </>;
   } else {
     child = <>
-      <TextEditor cell={cell}/>
-      <ActionBar id={cell.id}/>
+      <LayoutGroup>
+        <motion.div layout>
+          <TextEditor cell={cell}/>
+          <ActionBar id={cell.id}/>
+        </motion.div>
+      </LayoutGroup>
     </>;
   }
 
